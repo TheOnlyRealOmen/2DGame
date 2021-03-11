1e2a9b073b427f8bf1f58c4230e1d0f2c47e9d49
TheOnlyRealOmen <80462857+TheOnlyRealOmen@users.noreply.github.com>
2021-03-11 14:43:35 +0100

    Importing 2DGame

---
+++ 2DGame/Assets/Scripts/Collectibles/CollectibleScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleScript : MonoBehaviour
+{
+    protected Animator anim;
+
+    protected virtual void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Destroyed()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ 2DGame/Assets/Scripts/Collectibles/GemScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemScript : CollectibleScript
+{
+    private AudioSource coinSound;
+
+    private void Update()
+    {
+        coinSound = GetComponent<AudioSource>();
+    }
+    
+    public void Collected()
+    {
+        anim.SetTrigger("Collected");
+        coinSound.Play();
+    }
+
+    private void Destroyed()
+    {
+        Destroy(this.gameObject);
+    }
+}
