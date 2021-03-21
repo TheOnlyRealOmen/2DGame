e7fc623498b4672a4b3bf1ed93b19553e87c5921
JonathanLaptop <80462857+TheOnlyRealOmen@users.noreply.github.com>
2021-03-21 10:52:01 +0100

    Adding the CoinCounter & combining it with Healthbar

---
+++ 2DGame/Assets/Scripts/UI/CoinBarScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBarScript : MonoBehaviour
+{
+    private Animator anim;
+
+    private enum State {ZeroCoins, OneCoin, TwoCoins, ThreeCoins, FourCoins, FiveCoins}
+    private State state = State.ZeroCoins;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        AnimationState();
+        anim.SetInteger("state", (int)state);
+    }
+
+    private void AnimationState()
+    {
+        if (PermaUIScript.perm.gems == 0)
+        {
+            state = State.ZeroCoins;
+        }
+
+        if (PermaUIScript.perm.gems == 1)
+        {
+            state = State.OneCoin;
+        }
+
+        if (PermaUIScript.perm.gems == 2)
+        {
+            state = State.TwoCoins;
+        }
+
+        if (PermaUIScript.perm.gems == 3)
+        {
+            state = State.ThreeCoins;
+        }
+
+        if (PermaUIScript.perm.gems == 4)
+        {
+            state = State.FourCoins;
+        }
+
+        if (PermaUIScript.perm.gems == 5)
+        {
+            state = State.FiveCoins;
+        }
+    }
+}
